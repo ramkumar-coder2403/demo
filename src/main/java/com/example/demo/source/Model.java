30c5a3b88c6af7508356401969269c9a0747d09f
Ramkumar Megavarnan <67353220+ramkumar-coder2403@users.noreply.github.com>
2022-08-31 14:41:08 +0530

    Update Model.java

--- src/main/java/com/example/demo/source/Model.java
+++ src/main/java/com/example/demo/source/Model.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor @NoArgsConstructor
 public class Model {
-	private String title,c_id,description,ref_link;
-//	private int user_id;
-//	private String user_name,password;
+// 	private String title,c_id,description,ref_link;
+	private int user_id;
+	private String user_name,password;
 }
